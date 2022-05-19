282a82a3c3fe0c461d191ca86050137786290f30
Sam Itman <si237@njit.edu>
2022-05-19 18:41:49 -0400

    added home link on nav and style sheet

--- contact.php
+++ contact.php
@@ -1,2 +1,3 @@
 <?php require_once(__DIR__ . "/partials/nav.php");?>
+<link rel="stylesheet" href="/static/styles.css">
 <p>Contact information<p>
--- partials/nav.php
+++ partials/nav.php
@@ -85,8 +85,9 @@
 
 <nav>
     <ul>
-            <li><a href="resume.php">Resume</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
+            <li><a href="resume.php">Resume</a></li>
 	        <li><a href="contact.php">Contact</a></li>
     </ul>
 </nav>
