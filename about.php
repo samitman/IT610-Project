c000e5f6d5b0ad5e253088809391751bd8fba2ca
Sam Itman <si237@njit.edu>
2022-06-01 18:07:57 -0400

    new template

--- about.php
+++
@@ -1,3 +0,0 @@
-<?php require_once(__DIR__ . "/partials/nav.php");?>
-<link rel="stylesheet" href="/static/styles.css">
-<p>This is the About page<p>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php require_once(__DIR__ . "/partials/nav.php");?>
-<link rel="stylesheet" href="/static/styles.css">
-<p>Welcome to my website<p>
-<p>I am Sam!<p>
