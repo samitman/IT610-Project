6d50fd3cb143206f5a57db49a9bc296035a5edf5
Sam Itman <si237@njit.edu>
2022-06-15 10:29:38 -0400

    saved nav header into own file

--- index.php
+++ index.php
@@ -8,14 +8,7 @@
 
 	<div class="container">
 		<div class="navbar navbar-default navbar-static-top" role="navigation">
-		<div class="navbar-header">
-			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="icon icon-bar"></span>
-				<span class="icon icon-bar"></span>
-				<span class="icon icon-bar"></span>
-			</button>
-			<a href="#" class="navbar-brand"><img src="images/logo.png" class="img-responsive" alt="logo"></a>
-		</div>
+		<?php include("./partials/navbar_header.html");?>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="index.php" class="active">HOME</a></li>
